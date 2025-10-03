3a46ffba17e0f7859872e22d1fd916f6d16a6d7d
fede <fededimant@hotmail.com>
2025-10-02 21:28:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='porfaxxtrue', # Nombre de tu paquete
-    packages=['porfaxxtrue'],
+    name='yycore009', # Nombre de tu paquete
+    packages=['yycore009'],
     version='0.1', # Versión de tu paquete
     description='geoestadistica',
     author='fede',
@@ -10,3 +10,4 @@ setup(
     url='https://github.com/fededimant/yycore009', # URL del repositorio
     # Encuentra automáticamente los paquetes
   
+
